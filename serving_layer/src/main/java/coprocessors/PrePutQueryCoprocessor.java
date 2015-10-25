07d684f1e9381752efaf44621e1b354a1444f7f1
Nickolas Zoulis <nickolaszoulis@gmail.com>
2015-10-25 19:29:17 +0000

    added depencency of hbase-server

---
+++ serving_layer/src/main/java/coprocessors/PrePutQueryCoprocessor.java
@@ -0,0 +1,22 @@
+package coprocessors;
+
+import org.apache.hadoop.hbase.client.Durability;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.coprocessor.BaseRegionObserver;
+import org.apache.hadoop.hbase.coprocessor.ObserverContext;
+import org.apache.hadoop.hbase.coprocessor.RegionCoprocessorEnvironment;
+import org.apache.hadoop.hbase.regionserver.wal.WALEdit;
+
+import java.io.IOException;
+
+/**
+ * Created by nickozoulis on 25/10/2015.
+ */
+
+public class PrePutQueryCoprocessor extends BaseRegionObserver {
+
+    @Override
+    public void prePut(ObserverContext<RegionCoprocessorEnvironment> e, Put put, WALEdit edit, Durability durability) throws IOException {
+        super.prePut(e, put, edit, durability);
+    }
+}
