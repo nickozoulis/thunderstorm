4a3ff99d3fcfc0fffa34ecb6eb35f9411acac6ba
Nickolas Zoulis <nickolaszoulis@gmail.com>
2015-10-27 21:42:08 +0000

    making constants final

--- serving_layer/src/main/java/hbase/Cons.java
+++ serving_layer/src/main/java/hbase/Cons.java
@@ -6,19 +6,19 @@ package hbase;
 public class Cons {
 
     // HBase Configuration constants
-    public static String hbase_IP_address = "192.168.0.9";//127.0.0.1
-    public static String hbase_port = "2181";
+    public static final String hbase_IP_address = "192.168.0.9";//127.0.0.1
+    public static final String hbase_port = "2181";
 
     // HBase table constants
-    public static String queries = "queries";
-    public static String batch_views = "batch_views";
-    public static String stream_views = "stream_views";
-    public static String cfQueries = "q"; // column family for Queries table
-    public static String cfViews = "v"; // column family for batch and stream Views
-    public static String qid_ = "qid_";
-    public static String qid_0 = "qid_0";
-    public static String max_qid = "max_qid";
-    public static String clusters = "clusters";
-    public static String filter = "filter";
+    public static final String queries = "queries";
+    public static final String batch_views = "batch_views";
+    public static final String stream_views = "stream_views";
+    public static final String cfQueries = "q"; // column family for Queries table
+    public static final String cfViews = "v"; // column family for batch and stream Views
+    public static final String qid_ = "qid_";
+    public static final String qid_0 = "qid_0";
+    public static final String max_qid = "max_qid";
+    public static final String clusters = "clusters";
+    public static final String filter = "filter";
 
 }
