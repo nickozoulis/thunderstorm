19151cffb7cea8bef365c39379495c2d5a93a3b3
Nickolas Zoulis <nickolaszoulis@gmail.com>
2015-11-18 19:26:24 +0000

    removing deprecated classes

--- batch_layer/src/main/java/SparkKMeans.java
+++ batch_layer/src/main/java/SparkKMeans.java
@@ -14,7 +14,6 @@ import org.apache.spark.mllib.linalg.Vector;
 import org.apache.spark.mllib.linalg.Vectors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import java.io.IOException;
 
 
--- bridge/src/main/java/hbase/Cons.java
+++ bridge/src/main/java/hbase/Cons.java
@@ -22,7 +22,7 @@ public class Cons {
     public static final int delay = 3000; // Thread sleep delay in milliseconds
     public static final int batchDelay = 60000; // Every hour
     public static final int K = 10000; // The fixed number of clusterHeads
-    public static final String dataset = "/Users/nickozoulis/IdeaProjects/master_team_project/batch_layer/src/main/resources/input.txt";
+    public static final String dataset = "/input.txt";
     public static final int iterations = 10;
     public static final int runs = 1;
 }
