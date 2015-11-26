1be8942c435c9c0fadaa930e3ea810301d10f0d9
Nickolas Zoulis <nickolaszoulis@gmail.com>
2015-11-26 18:07:35 +0000

    reformating code and implementing experiment procedure

--- serving_layer/src/main/java/experiment/ExperimentDriver.java
+++ serving_layer/src/main/java/experiment/ExperimentDriver.java
@@ -1,7 +1,17 @@
 package experiment;
 
+import hbase.Cons;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HColumnDescriptor;
+import org.apache.hadoop.hbase.HTableDescriptor;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.*;
+import org.apache.hadoop.hbase.util.Bytes;
+import org.apache.log4j.Logger;
 import shell.HBaseUtils;
+import shell.ShellUtils;
 
+import java.io.IOException;
 import java.util.List;
 
 /**
@@ -9,11 +19,58 @@ import java.util.List;
  */
 public class ExperimentDriver {
 
+    private static final Logger logger = Logger.getLogger(ExperimentDriver.class);
+    private static HConnection connection;
 
     public ExperimentDriver(List<String> queryList) {
         HBaseUtils.setHBaseConfig();
+        Configuration conf = HBaseUtils.getHBaseConfig();
+        try {
+            connection = HConnectionManager.createConnection(conf);
+
+            cleanup();
+
+            experiment(queryList);
+
+        } catch (IOException e) {e.printStackTrace();}
+    }
+
+    private void experiment(List<String> queryList) {
+        for (String line : queryList) {
+            String[] splits = line.split(" ");
+            ShellUtils.KMeans(ShellUtils.parseKMeans(line, splits));
+        }
+    }
 
 
+    /**
+     * Cleans up the queries table, making it ready for experiment.
+     */
+    private static void cleanup() {
+        Configuration conf = connection.getConfiguration();
+        logger.info("Preparing a new data stream simulation.");
 
+        try {
+            HBaseAdmin admin = new HBaseAdmin(conf);
+            admin.disableTable(Cons.queries);
+            admin.deleteTable(Cons.queries);
+            logger.info("Table deleted: " + Cons.queries);
+
+            HTableDescriptor tableDescriptor = new HTableDescriptor(TableName.valueOf(Cons.queries));
+            tableDescriptor.addFamily(new HColumnDescriptor(Cons.cfQueries));
+
+            HTable hTable = new HTable(conf, Cons.queries);
+            Put p = new Put(Bytes.toBytes(0l));
+            p.add(Bytes.toBytes(Cons.cfQueries),
+                    Bytes.toBytes(Cons.max_qid), Bytes.toBytes(0l)); // Zero as Long
+            hTable.put(p);
+
+            admin.createTable(tableDescriptor);
+            logger.info("Table created: " + tableDescriptor.getNameAsString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        logger.info("Data stream simulation is ready to begin.");
     }
 }
--- serving_layer/src/main/java/shell/Shell.java
+++ serving_layer/src/main/java/shell/Shell.java
@@ -1,26 +1,18 @@
 package shell;
 
-import clustering.KMeansQuery;
-import clustering.LocalKMeans;
-import clustering.QueryType;
 import experiment.ExperimentDriver;
 import hbase.Cons;
 import jline.TerminalFactory;
 import jline.console.ConsoleReader;
 import joptsimple.OptionParser;
 import joptsimple.OptionSet;
-import net.sf.javaml.core.Dataset;
 import org.apache.hadoop.hbase.client.Result;
-import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.log4j.Logger;
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -84,7 +76,7 @@ public class Shell {
                 get(splits);
                 break;
             case "kmeans":
-                parseKMeans(line, splits);
+                ShellUtils.KMeans(ShellUtils.parseKMeans(line, splits));
                 break;
             case "test":
                 HBaseUtils.testConnectionWithHBase(HBaseUtils.getHBaseConfig());
@@ -108,11 +100,12 @@ public class Shell {
                 System.exit(0);
                 break;
             default:
-                usage();
+                try {
+                    console.println(ShellUtils.kmeansUsage());
+                } catch (IOException e) {e.printStackTrace();}
                 break;
         }
     }
-
 
     private void get(String[] splits) {
         long qid;
@@ -128,14 +121,14 @@ public class Shell {
             case "batch":
                 r = HBaseUtils.getRowFromBatchViews(qid);
                 if (r != null)
-                    printResultView(r);
+                    ShellUtils.printResultView(r);
                 else
                     System.out.println("No batch view for qid: " + qid);
                 break;
             case "stream":
                 r = HBaseUtils.getRowFromStreamViews(qid);
                 if (r != null)
-                    printResultView(r);
+                    ShellUtils.printResultView(r);
                 else
                     System.out.println("No stream view for qid: " + qid);
                 break;
@@ -143,29 +136,6 @@ public class Shell {
                 System.err.println("Example use: get [batch/stream] [qid]");
                 break;
         }
-    }
-
-    private void printResultView(Result result) {
-        byte[] valueClusters;
-        int k = 0;
-
-        System.out.println();
-        for (;;) {
-            valueClusters = result.getValue(Bytes.toBytes(Cons.cfViews), Bytes.toBytes(Cons.clusters_ + k));
-
-            if (valueClusters != null)
-                System.out.println(Bytes.toString(valueClusters));
-            else
-                break;
-
-            k++;
-        }
-        System.out.println();
-    }
-
-    private void printResultDataset(Dataset[] datasets) {
-        for (Dataset d : datasets)
-            System.out.println(d.instance(0));
     }
 
     private void clearScreen() {
@@ -241,65 +211,6 @@ public class Shell {
         }
     }
 
-    private void parseKMeans(String line, String[] splits) {
-        KMeansQuery query;
-
-        if (splits.length == 2) { // Plain KMeans
-            query = new KMeansQuery(Integer.parseInt(splits[1]));
-        } else if (splits.length > 2) { // Constrained KMeans
-            query = parseConstraints(line);
-        } else {
-            usage();
-            return;
-        }
-
-        //FIXME
-        if (query != null) KMeans(query);
-    }
-
-    /**
-     * Shows explanatory message when incorrect input is inserted.
-     */
-    private void usage() {
-        try {
-            console.println("Example input: 'kmeans 4' or 'kmeans 4 ; x0+x1<6'");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private KMeansQuery parseConstraints(String line) {
-        String constraintExpr = "", clustersExpr = "", operator = "", numOfClusters = "";
-        String pattern1 = "(.*);(.*)",
-                pattern2 = "kmeans(\\s*)(\\d+)(\\s*)";
-
-        // Get constraint and clusters expressions
-        Pattern r = Pattern.compile(pattern1);
-        Matcher m = r.matcher(line);
-        //FIXME: Make it work for more than one filter.
-        if (m.find()) {
-            clustersExpr = m.group(1);
-            constraintExpr = m.group(2);
-        } else {
-            usage();
-            return null;
-        }
-
-        // Get numOfClusters
-        r = Pattern.compile(pattern2);
-        m = r.matcher(clustersExpr);
-        if (m.find()) {
-            numOfClusters = m.group(2);
-
-            Set set = new HashSet<>();
-            set.add(constraintExpr);
-
-            return new KMeansQuery(Integer.parseInt(numOfClusters), set);
-        }
-
-        return null;
-    }
-
     public static void main(String[] args) {
         OptionParser parser = new OptionParser("f:");
         OptionSet options = parser.parse(args);
@@ -309,64 +220,6 @@ public class Shell {
         } else { // Normal mode
             new Shell();
         }
-    }
-
-    /**
-     * Performs the whole Lambda-KMeans procedure.
-     *
-     * @param query
-     */
-    private void KMeans(KMeansQuery query) {
-        Result r;
-
-        // Check if query exists in Queries table.
-        long queryRowKey = HBaseUtils.getQueryIDIfExists(query);
-
-        // If no, add it to HBase
-        if (queryRowKey == -1)
-            HBaseUtils.putKMeansQuery(query);
-
-        // Check stream views if contain results for this query. Assuming stream takes batch views as input.
-        r = HBaseUtils.getRowFromStreamViews(queryRowKey);
-
-        // If yes, return it to the user.
-        if (!r.isEmpty()) {
-            printResultView(r);
-            return;
-        }
-
-        /*
-            While these layers are computing, check whether there is a view for k'-means
-            (e.g., k'=10,000) for the same set of constraints
-        */
-        KMeansQuery kQuery = null;
-        if (query.getQueryType() == QueryType.KMEANS)
-            kQuery = new KMeansQuery(Cons.K);
-        else if (query.getQueryType() == QueryType.CONSTRAINED_KMEANS)
-            kQuery = new KMeansQuery(Cons.K, query.getFilters());
-        // Check whether this kQuery already exists
-
-        long kQueryRowKey = HBaseUtils.getQueryIDIfExists(kQuery);
-
-        // If no, add it to HBase
-        if (kQueryRowKey == -1)
-            HBaseUtils.putKMeansQuery(kQuery);
-
-        // Check stream views if contain results for this kQuery.
-        r = HBaseUtils.getRowFromStreamViews(kQueryRowKey);
-
-        // If yes, then compute a Local k-out-of-k'-means clustering and return that to the user
-        if (!r.isEmpty()) {
-            printResultDataset(new LocalKMeans(query, HBaseUtils.loadClusters(r)).cluster());
-            return;
-        }
-
-
-        /*
-            While they are computing, we retrieve the k'-means view for the whole dataset (i.e., no constraints),
-            and compute and return a k-out-of-k'-means result to the user.
-        */
-
     }
 
 }
